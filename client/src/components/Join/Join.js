c4047a10de69004a72e275879e33f0071d9ec973
Laurent <laurent.callens@epitech.eu>
2020-06-16 12:31:23 +0200

    ajout bootstrap

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -7,17 +7,25 @@ const Join = () => {
     const [room, setRoom] = useState('');
 
     return (
-        <div className="JoinOuterContainer">
-            <div className="JoinInnerContainer">
-                <h1 className="heading">Welcome to IRC by L & R !!  </h1>
-                <h2 className="heading">Become User of chat register by filling in the fields</h2>
-                <div><input className="joinInput" placeholder="Name" className="JoinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
-                <div><input className="joinInput" placeholder="Room" className="JoinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
-                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <button className="button mt-20" type="submit"> start Chat with some friends </button>
-                </Link>
-            </div>
-        </div>
+<div className='container'>
+	<blockquote class="blockquote text-center mt-5">
+	  <p class="mb-0">Bienvenue sur my_IRC</p>
+	  <p class="blockquote-footer"> Veuillez entrer votre pseudo ainsi que la room dans laquelle aller </p>
+	</blockquote>
+	<form>
+	  <div className="form-group">
+	    <label>Pseudo</label>
+	    <input type="text" className="form-control" id="username" onChange={(event) => setName(event.target.value)}/>
+	  </div>
+	  <div className="form-group">
+	    <label>Room</label>
+	    <input type="text" className="form-control" id="room" onChange={(event) => setRoom(event.target.value)}/>
+	  </div>
+		<Link onClick={event => (!name || !room) ? event.preventDefault() : null } to={`/chat?name=${name}&room=${room}`}>
+			<button className="btn btn-primary button" type="submit">Go !</button>
+		</Link>
+	</form>
+</div>
         )
     }
 
